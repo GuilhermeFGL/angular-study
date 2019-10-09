e07af1a757d86efeb0bb855924b9d2d5aede4954
GuilhermeFGL <guilherme_fgl@hotmail.com>
2019-10-09 13:55:44 -0300

    angular components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-study';
+  student1 = {name: "Joao", isActive: true, class: "Class A"};
+  student2 = {name: "Maria", isActive: true, class: "Class B"};
+  student3 = {name: "Fred", isActive: false};
 }
